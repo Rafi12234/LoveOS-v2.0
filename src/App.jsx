f1e028bd736437f49f95571f80098a863aa62295
Rafi12234 <shajidulkabir12345@gmail.com>
2026-03-12 02:25:06 +0600

    feat: scaffold root App component

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="relative min-h-screen overflow-hidden">
+      <h1 className="text-white text-center pt-20 text-4xl">LoveOS v2.0</h1>
+    </div>
+  );
+}
